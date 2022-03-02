b0eaf9fb54b34746bb07fd4c37b5ae6b32c78e9d
GabrielInacio03 <inaciogs03@gmail.com>
2022-03-02 07:59:32 -0300

    polimorfismo

--- AplicacaoConsole/Classes/Cliente.cs
+++ AplicacaoConsole/Classes/Cliente.cs
@@ -27,42 +27,24 @@ namespace AplicacaoConsole.Classes
         {
             this.Olhar();
 
-            if(this.GetType() == typeof(Cliente))
+           
+            var clientes = Cliente.LerClientes();
+            clientes.Add(this);
+
+            if (File.Exists(CaminhoBaseClientes()))
             {
-                var clientes = Cliente.LerClientes();
-                clientes.Add(this);
+                StreamWriter r = new StreamWriter(CaminhoBaseClientes());
+                r.WriteLine("nome;telefone;cpf");
 
-                if (File.Exists(CaminhoBaseClientes()))
+                foreach (Cliente c in clientes)
                 {
-                    StreamWriter r = new StreamWriter(CaminhoBaseClientes());
-                    r.WriteLine("nome;telefone;cpf");
-
-                    foreach (Cliente c in clientes)
-                    {
-                        var linha = c.Nome +";"+ c.Telefone +";"+ c.Cpf +";";
-                        r.WriteLine(linha);
-                    }
-                    r.Close();
+                    var linha = c.Nome +";"+ c.Telefone +";"+ c.Cpf +";";
+                    r.WriteLine(linha);
                 }
+                r.Close();
             }
-            else
-            {
-                var usuarios = Usuario.LerUsuarios();
-                usuarios.Add((Usuario)this);
-
-                if (File.Exists(CaminhoBaseUsuarios()))
-                {
-                    StreamWriter r = new StreamWriter(CaminhoBaseUsuarios());
-                    r.WriteLine("nome;telefone;cpf");
-
-                    foreach (Usuario c in usuarios)
-                    {
-                        var linha = c.Nome + ";" + c.Telefone + ";" + c.Cpf + ";";
-                        r.WriteLine(linha);
-                    }
-                    r.Close();
-                }
-            }
+            
+           
             
         }
         private void Olhar()
@@ -72,33 +54,8 @@ namespace AplicacaoConsole.Classes
         private static string CaminhoBaseClientes()
         {
             return ConfigurationManager.AppSettings["base_dos_clientes"];
-        }
-        private static string CaminhoBaseUsuarios()
-        {
-            return ConfigurationManager.AppSettings["base_dos_usuarios"];
-        }
-        public static List<Usuario> LerUsuarios()
-        {
-            var usuarios = new List<Usuario>();
-            if (File.Exists(CaminhoBaseUsuarios()))
-            {
-                using (StreamReader arquivo = File.OpenText(CaminhoBaseUsuarios()))
-                {
-                    string linha;
-                    int i = 0;
-                    while((linha = arquivo.ReadLine()) != null)
-                    {
-                        i++;
-                        if (i == 1) continue;
-                        var usuarioArquivo = linha.Split(';');
-                        var usuario = new Usuario { Nome = usuarioArquivo[0], Telefone = usuarioArquivo[1], Cpf = usuarioArquivo[2] };
-
-                        usuarios.Add(usuario);
-                    }
-                }
-            }
-            return usuarios;
-        }
+        }        
+        
         public static List<Cliente> LerClientes()
         {
             var clientes = new List<Cliente>();
--- AplicacaoConsole/Classes/Usuario.cs
+++ AplicacaoConsole/Classes/Usuario.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +16,56 @@ namespace AplicacaoConsole.Classes
             this.Nome = nome;
             this.Telefone = telefone;
             this.Cpf = cpf;
+        }
+        public void Gravar()
+        {
+            this.Olhar();
+
+            var usuarios = Usuario.LerUsuarios();
+            usuarios.Add((Usuario)this);
+
+            if (File.Exists(CaminhoBaseUsuarios()))
+            {
+                StreamWriter r = new StreamWriter(CaminhoBaseUsuarios());
+                r.WriteLine("nome;telefone;cpf");
+
+                foreach (Usuario c in usuarios)
+                {
+                    var linha = c.Nome + ";" + c.Telefone + ";" + c.Cpf + ";";
+                    r.WriteLine(linha);
+                }
+                r.Close();
+            }
+        }
+        private static string CaminhoBaseUsuarios()
+        {
+            return ConfigurationManager.AppSettings["base_dos_usuarios"];
+        }
+        public static List<Usuario> LerUsuarios()
+        {
+            var usuarios = new List<Usuario>();
+            if (File.Exists(CaminhoBaseUsuarios()))
+            {
+                using (StreamReader arquivo = File.OpenText(CaminhoBaseUsuarios()))
+                {
+                    string linha;
+                    int i = 0;
+                    while ((linha = arquivo.ReadLine()) != null)
+                    {
+                        i++;
+                        if (i == 1) continue;
+                        var usuarioArquivo = linha.Split(';');
+                        var usuario = new Usuario { Nome = usuarioArquivo[0], Telefone = usuarioArquivo[1], Cpf = usuarioArquivo[2] };
+
+                        usuarios.Add(usuario);
+                    }
+                }
+            }
+            return usuarios;
+        }
+        private void Olhar()
+        {
+            Console.WriteLine("O cliente " + this.Nome + " " + this.SobreNome + " está olhando para mim");
         }
     }
 }
